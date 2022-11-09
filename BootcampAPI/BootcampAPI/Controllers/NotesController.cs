bf9844f1cf9681da302bbf43c9f42152e778b314
KAMILKAPLAN\KAMILKAPLAN <kamilkaplnn@gmail.com>
2022-11-09 19:32:37 +0300

    NoteController

---
+++ BootcampAPI/BootcampAPI/Controllers/NotesController.cs
@@ -0,0 +1,104 @@
+﻿using BootcampAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BootcampAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotesController : Controller
+    {
+        private readonly BackEndContext _context;
+        public NotesController()
+        {
+            _context = new BackEndContext();
+        }
+
+        // GET: api/Notes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Notes>>> GetNotes()
+        {
+            return await _context.Notes.ToListAsync();
+        }
+
+        // GET: api/Notes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Notes>> GetNotes(int id)
+        {
+            var notes = await _context.Notes.FindAsync(id);
+
+            if (notes == null)
+            {
+                return NotFound();
+            }
+
+            return notes;
+        }
+
+        // PUT: api/Notes/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutNotes(int id, Notes notes)
+        {
+            if (id != notes.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(notes).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!NotesExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Notes
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Notes>> PostNotes(Notes notes)
+        {
+            _context.Notes.Add(notes);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetNotes", new { id = notes.Id }, notes);
+        }
+
+        // DELETE: api/Notes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteNotes(int id)
+        {
+            var notes = await _context.Notes.FindAsync(id);
+            if (notes == null)
+            {
+                return NotFound();
+            }
+
+            _context.Notes.Remove(notes);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool NotesExists(int id)
+        {
+            return _context.Notes.Any(e => e.Id == id);
+        }
+    }
+}
--- CSharpLearn/CSharpBootcamp/Program.cs
+++ CSharpLearn/CSharpBootcamp/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] names)
         {
-            
+            SimpleClass sc = new SimpleClass();
         }
+
+        public class SimpleClass
+        {
+            SimpleClass sc;
+            public SimpleClass()
+            {
+                sc= new SimpleClass();
+            }
+        }
+        /*
+         * Stack overflow.
+         * Repeat 19264 times:
+         * --------------------------------
+         *    at CSharpBootcamp.Program+SimpleClass..ctor()
+         * --------------------------------
+         *    at CSharpBootcamp.Program.Main(System.String[])
+         */
     }
 }
